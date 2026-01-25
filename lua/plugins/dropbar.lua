de5bd159765a1431303bfa0bc242e9ad16280852
Eiji Meguro <eiji@fork.co.jp>
2026-01-25 10:38:02 +0900

    feat: dropbar.lua を追加
    コード構造情報を基に、カーソル移動できるナビを提供

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,17 @@
+-- ~/.config/nvim/lua/plugins/dropbar.lua
+return {
+  {
+    'Bekaboo/dropbar.nvim',
+    dependencies = {
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      -- 好みのキーマップを設定（例）
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  }
+}
+
