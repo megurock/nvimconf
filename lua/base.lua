67b0c468edcfb7fb12da6f32c81430dc53f1fd8f
Eiji Meguro <eiji@fork.co.jp>
2026-01-25 13:35:13 +0900

    feat: 危険な Unicode 空白をハイライト
    不用意な空白文字をハイライトする設定を base.lua に設定

--- lua/base.lua
+++ lua/base.lua
@@ -15,3 +15,17 @@ vim.o.foldcolumn = '1'
 -- リーダーの設定
 vim.g.mapleader = ' '
 
+-- 危険な Unicode 空白をハイライト
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    vim.fn.matchadd(
+      "ExtraWhitespace",
+      "[\u{00A0}\u{2000}-\u{200B}\u{3000}]"
+    )
+    vim.api.nvim_set_hl(0, "ExtraWhitespace", {
+      underline = true,
+      sp = "#ff0088",  -- 下線の色
+    })
+  end,
+})
