d0701fb5a9e2a1c0e946bc50082a8baab3d1525d
Eiji Meguro <eiji@fork.co.jp>
2026-01-25 13:45:33 +0900

    feat: lazy-tree へ直接移動するショートカットキーを追加
    - <leader>e: tree をトグル
    - <leadr>E: tree へ移動

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,13 +2,30 @@ return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
-  keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Explorer" },
-  },
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
+  keys = {
+    -- トグル（今まで通り）
+    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Explorer" },
+
+    -- フォーカス or 開く
+    {
+      "<leader>E",
+      function()
+        local api = require("nvim-tree.api")
+
+        if api.tree.is_visible() then
+          api.tree.focus()
+        else
+          api.tree.open()
+        end
+      end,
+      desc = "Focus File Explorer (open if closed)",
+    },
+  },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup({})
   end,
 }
+
