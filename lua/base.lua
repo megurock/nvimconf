b026eff7e174bbfd6a9159f2f45e368b4be8cacd
Eiji Meguro <eiji@fork.co.jp>
2026-01-25 10:54:01 +0900

    feat(base): neovim のfold 設定を追加
    - za でブロックを開閉トグル

--- lua/base.lua
+++ lua/base.lua
@@ -6,4 +6,12 @@ vim.o.shiftwidth = 2
 vim.o.termguicolors = true
 vim.o.ruler = true
 
+-- fold を有効化
+vim.o.foldmethod = 'indent'
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldcolumn = '1'
+
+-- リーダーの設定
 vim.g.mapleader = ' '
+
