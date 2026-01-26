625813e7082365198941cca5492f7b9f112a512d
Eiji Meguro <eiji@fork.co.jp>
2026-01-26 21:08:27 +0900

    feat(telescope): Telescope のショートカットを変更
    - ff: Find Files
    - fg: Live Grep
    - fb: Buffers
    - fh: Help Tags

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,11 +6,26 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   keys = {
-    { "<leader>p", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
-    { "<leader>g", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Help Tags" },
   },
   config = function()
-    require("telescope").setup {}
+    local actions = require("telescope.actions")
+
+    require("telescope").setup {
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-d>"] = actions.preview_scrolling_down,
+            ["<C-u>"] = actions.preview_scrolling_up,
+          },
+        },
+      },
+    }
+
     pcall(require("telescope").load_extension, "fzf")
   end,
 }
+
