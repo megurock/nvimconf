2109636db3510310977e4ce4b69f949dc41590d7
Eiji Meguro <eiji@fork.co.jp>
2026-02-24 21:19:01 +0900

    feat: neovim-tree で html ファイルをブラウザで開く機能を追加
    - ファイルエクスプローラで html ファイルを選択中に <leader> B 入力

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,11 +5,8 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  filters = {
-    custom = { ".DS_Store" },
-  },
   keys = {
-    -- トグル（今まで通り）
+    -- トグル
     { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle File Explorer" },
 
     -- フォーカス or 開く
@@ -28,11 +25,45 @@ return {
     },
   },
   config = function()
+    local function open_in_browser(node)
+      local path = node.absolute_path
+      if not path then
+        return
+      end
+
+      -- ディレクトリは無視
+      if node.type == "directory" then
+        return
+      end
+
+      -- macOS
+      vim.fn.jobstart({ "open", path }, { detach = true })
+
+      -- Linux の場合
+      -- vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+
+      -- Windows の場合
+      -- vim.fn.jobstart({ "cmd.exe", "/c", "start", path }, { detach = true })
+    end
+
     require("nvim-tree").setup({
       filters = {
         custom = { ".DS_Store" },
       },
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- B でブラウザ表示
+        vim.keymap.set("n", "B", function()
+          local node = api.tree.get_node_under_cursor()
+          open_in_browser(node)
+        end, {
+          buffer = bufnr,
+          desc = "Open in browser",
+        })
+      end,
     })
   end,
 }
-
